05f4da17281c31a767909d3a12580ba0834e1904
mlync87 <martylynch35@hotmail.com>
2023-05-04 09:12:46 +0100

    updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import { Convas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor="black" />
+    </mesh>
     <div>Computers</div>
   )
 }

f4fe578c1f6e67cbd65d5281c81fa5d48db200c3
mlync87 <martylynch35@hotmail.com>
2023-06-17 16:20:32 +0100

    navbar plans

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+// change fonts to cursive and have a look at the colour scheme. Perhamps have some contrast
 
 const Navbar = () => {
   const [active, setActive] = useState("'");

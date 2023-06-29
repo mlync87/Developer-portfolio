a1f7e3ff55ad43580640ad6615f68ae0816900e2
mlync87 <martylynch35@hotmail.com>
2023-06-29 11:05:28 +0100

    nots for timeline and expectations

--- src/components/index.js
+++ src/components/index.js
@@ -33,3 +33,4 @@ export {
 // condense images sent by maria
 // change colour palette and fade from left to right in background
 // create custom images for services offered"
+// establish time frame for completion of project
